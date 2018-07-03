5acd5608fdfabdb0f09399651dec0b9ef5f4ba2d
Ambrose Hundal <amshunda@ucsc.edu>
2018-07-03 01:59:14 -0700

    Ruby on Rails Web_BLOG_APP with comments

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+ http_basic_authenticate_with name: "ambreezy", password: "developer", except: [:index, :show]
+
+ def index
+    @articles = Article.all
+ end
+
+ def show
+    @article = Article.find(params[:id])
+ end
+
+ def new
+ end
+ 
+ def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    @article.save
+    redirect_to @article
+ end
+end
